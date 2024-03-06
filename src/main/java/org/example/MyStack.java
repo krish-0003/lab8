25c27ef5772026791dbc543bb2296f864ddd4c4b
krish-0003 <gondaliyakrish0003@gmail.com>
2024-03-06 18:40:16 -0500

    Fixed pop() method in MyStack class to handle empty stacks correctly

--- src/main/java/org/example/MyStack.java
+++ src/main/java/org/example/MyStack.java
@@ -8,8 +8,9 @@ public class MyStack<E> extends ArrayList<E> {
     public void push(E e) {
         add(e);
     }
-    public E pop() {
 
+    public E pop() {
+        if (isEmpty()) throw new EmptyStackException();
         E e = get(size() - 1);
         remove(size() -1 );
         return e;
